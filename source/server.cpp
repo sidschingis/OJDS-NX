8245c79e1dbeaaa63c67e76fda64c76f80f5342c
sidschingis <SijingYou92@gmail.com>
2020-11-01 12:08:44 +0100

    Remove Jansson Dependency

--- source/server.cpp
+++ source/server.cpp
@@ -16,9 +16,8 @@
  * You should have received a copy of the GNU General Public License
  * along with OJDS-NX. If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 #include <arpa/inet.h>
-#include <jansson.h>
 #include <limits.h>
 #include <sys/socket.h>
 
@@ -26,7 +25,8 @@
 
 using namespace std;
 
-int setupServerSocket() {
+int setupServerSocket()
+{
     auto server_sock = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in server;
@@ -34,8 +34,9 @@ int setupServerSocket() {
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(56709);
 
-    auto addr = (struct sockaddr *) &server;
-    while (bind(server_sock, addr, sizeof(server)) < 0) {
+    auto addr = (struct sockaddr *)&server;
+    while (bind(server_sock, addr, sizeof(server)) < 0)
+    {
         svcSleepThread(1e+9L);
     }
 
@@ -44,38 +45,108 @@ int setupServerSocket() {
     return server_sock;
 }
 
-string buildJSONPayload(u64 keys, JoystickPosition lPos, JoystickPosition rPos) {
-    auto root = json_object();
-    auto axes = json_array();
-    auto buttons = json_array();
+class Axes
+{
+public:
+    float lx;
+    float ly;
+    float rx;
+    float ry;
 
-    json_object_set_new(root, "axes", axes);
-    json_object_set_new(root, "buttons", buttons);
-    json_object_set_new(root, "connected", json_true());
-    json_object_set_new(root, "id", json_string("Nintendo Switch"));
-    json_object_set_new(root, "index", json_integer(0));
-    json_object_set_new(root, "mapping", json_string("standard"));
-    json_object_set_new(root, "timestamp", json_real(0));
+    string toJson()
+    {
+        string json = string("[") + std::to_string(lx) + "," + std::to_string(ly) + "," + std::to_string(rx) + "," + std::to_string(ry) + "]";
 
-    json_array_append_new(axes, json_real((double) lPos.dx / (double) SHRT_MAX));
-    json_array_append_new(axes, json_real((double) lPos.dy * -1 / (double) SHRT_MAX));
-    json_array_append_new(axes, json_real((double) rPos.dx / (double) SHRT_MAX));
-    json_array_append_new(axes, json_real((double) rPos.dy * -1 / (double) SHRT_MAX));
+        return json;
+    }
+};
+class Button
+{
+public:
+    bool pressed;
+    int value;
 
-    for (int i = 0; i < 16; i++) {
-        bool keyPressed = keys & BIT(i);
-        auto button = json_object();
-        json_object_set_new(button, "pressed", json_boolean(keyPressed));
-        json_object_set_new(button, "value", json_integer((keyPressed) ? 1 : 0));
-        json_array_append_new(buttons, button);
+    string toJson()
+    {
+        string json = string("{") +
+                      string("\"pressed\" : ") + std::to_string(pressed) + string(",") +
+                      string("\"value\"  : ") + std::to_string(value) +
+                      string("}");
+
+        return json;
+    }
+};
+
+class Buttons
+{
+public:
+    string buttonJson = string("");
+
+    void addButton(Button button, int i)
+    {
+        if (i != 0)
+        {
+            buttonJson = buttonJson + string(",");
+        }
+
+        buttonJson = buttonJson + button.toJson();
     }
 
-    auto json = json_dumps(root, JSON_COMPACT);
-    auto result = string(json);
+    string toJson()
+    {
+        string json = string("[") + buttonJson + string("]");
 
-    free(json);
-    json_decref(root);
+        return json;
+    }
+};
 
+class JsonPayload
+{
+public:
+    Axes axes;
+    Buttons buttons;
+
+    string toJson()
+    {
+        string json = string("{") +
+                      string("\"axes\" : ") + axes.toJson() + string(",") +
+                      string("\"buttons\" : ") + buttons.toJson() + string(",") +
+                      string("\"connected\" : true,") +
+                      string("\"id\" : \"Nintendo Switch\",") +
+                      string("\"index\" : 0,") +
+                      string("\"mapping\" : \"standard\",") +
+                      string("\"timestamp\" : 0.0") +
+                      string("}");
+
+        return json;
+    }
+};
+
+string buildJSONPayload(u64 keys, JoystickPosition lPos, JoystickPosition rPos)
+{
+    JsonPayload payload;
+
+    Axes axes;
+    axes.lx = (double)lPos.dx / (double)SHRT_MAX;
+    axes.ly = (double)lPos.dy * -1 / (double)SHRT_MAX;
+    axes.rx = (double)rPos.dx / (double)SHRT_MAX;
+    axes.ry = (double)rPos.dy * -1 / (double)SHRT_MAX;
+    payload.axes = axes;
+
+    Buttons btns;
+
+    for (int i = 0; i < 16; i++)
+    {
+        bool keyPressed = keys & BIT(i);
+        Button button;
+        button.pressed = keyPressed;
+        button.value = keyPressed ? 1 : 0;
+        btns.addButton(button, i);
+    }
+
+    payload.buttons = btns;
+
+    auto result = payload.toJson();
     result.insert(0, to_string(result.size()) + "#");
 
     return result;
